392d5313477e20abd97f4c8f42a251dbdc09ae92
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2022-04-24 00:13:12 -0300

    feat(context): updating websitepage

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,30 +1,64 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 import styled, { css } from 'styled-components';
+import Link from '../../components/commons/Link';
 import Text from '../../components/foundations/Text';
 
 import PageWrapper from '../../components/layout/PageWrapper';
 
 import breakpointMedia from '../../theme/utils/breakpointMedia';
 
-const HtmlComponent = styled.div`
+const IntroductionCard = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width:100%;
+  height: 75vh;
   /* ${breakpointMedia({
     md: css`
     `,
   })} */
 `;
 
+const LinksCard = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  /* width:100%; */
+  ${breakpointMedia({
+    md: css`
+    flex-direction: row;
+    `,
+  })}
+`;
+
+const linksList = [
+  {
+    title: 'My Work',
+    link: 'work',
+  },
+  {
+    title: 'About Me',
+    link: 'about',
+  },
+];
+
 const Home = () => (
   <PageWrapper>
-    <HtmlComponent>
-      <Text variant="title" color="#fff">
-        Boiler Plate
+    <IntroductionCard>
+      <Text variant="title" tag="h1">
+        Hello, world! I'm Gabriel.
       </Text>
-    </HtmlComponent>
+      <Text variant="subTitle" tag="h3">
+        I build stuff for the internet.
+      </Text>
+      <LinksCard>
+        {linksList.map((item) => <Link href={item.link}>{item.title}</Link>)}
+      </LinksCard>
+    </IntroductionCard>
   </PageWrapper>
 );
 
