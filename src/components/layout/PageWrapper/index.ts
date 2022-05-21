74cf433fd83437f4fc406136fe29b4c02e9fff65
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2022-05-21 12:25:09 -0300

    feat(ui): adding styles to PageWrapper

--- src/components/layout/PageWrapper/index.ts
+++ src/components/layout/PageWrapper/index.ts
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 const PageWrapper = styled.div`
-padding: 0 30px
+width: 100%;
+padding: 0 30px;
+display: flex;
+flex-direction: column;
+justify-content:center;
+align-items: center;
 `;
 
 export default PageWrapper;
